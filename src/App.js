d60d7c9858b9bcc60b7d4899caa4f3c80a2f03fc
Akash6790 <akashuk679@gmail.com>
2025-09-21 23:57:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/about';
+import Navbar from './components/navnar';
+import Textforms from './components/textforms';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="Textutils"/>
+    <div className="container">
+       {/* <Textforms heading="Enter you text here :"/> */}
     </div>
+    <div className="container">
+        <About  />
+    </div>
+   
+    </>
   );
 }
 
---
+++ src/components/textforms.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+export default function Textforms(props) {
+    const handleupclick = () => {
+        console.log("button was clicked" + text);
+        let newtext = text.toUpperCase();
+        settext(newtext)
+    }
+    const handleloclick = () => {
+        console.log("button was clicked" + text);
+        let newtext = text.toLowerCase();
+        settext(newtext)
+    }
+    const handleclearclick = () => {
+        console.log("button was clicked" + text);
+        let newtext = " ";
+        settext(newtext)
+    }
+    const sentenceCase = () => {
+        let sentences = text.split(/(?<=\.)\s+/);
+        let newText = sentences
+            .map(sentence => sentence.charAt(0).toUpperCase() + sentence.slice(1))
+            .join("\n ");
+        settext(newText);
+    };
+    const handleonchange = (event) => {
+        console.log("on change")
+        settext(event.target.value)
+    }
+    const [text, settext] = useState('Enter text');
+    return (
+        <>
+            <div className="container mb-3">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control  border-primary" onChange={handleonchange} value={text} id="box1" rows="8"> enter the text</textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleupclick} type="submit">Convert to upppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleloclick} type="submit">Convert to lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={sentenceCase} type="submit">Convert to sentenceCase</button>
+                <button className="btn btn-primary" onClick={handleclearclick} type="submit">Clear Text</button>
+            </div>
+            <div className="container ">
+                <h2>Your summary</h2>
+                <p>{text.split(" ").length} words, {text.length} characters</p>
+                <h3>Preview</h3>
+                <p>{0.08 * text.split(" ").length} Minutes to Read</p>
+                <div className="container  border border-primary p-3">
+                    <p>{text}</p>
+                </div>
+
+            </div>
+        </>
+    )
+}
