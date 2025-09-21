eb83c761ba936d6cdb6019829c46fa69d91194b8
Akash6790 <akashuk679@gmail.com>
2025-09-22 00:51:56 +0530

    adding about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import About from './components/about';
 import Navbar from './components/navnar';
-import Textforms from './components/textforms';
+// import Textforms from './components/textforms';
 
 function App() {
   return (
     <>
     <Navbar title="Textutils"/>
-    <div className="container">
-       {/* <Textforms heading="Enter you text here :"/> */}
-    </div>
+    {/* <div className="container">
+       <Textforms heading="Enter you text here :"/>
+    </div> */}
     <div className="container">
         <About  />
     </div>
